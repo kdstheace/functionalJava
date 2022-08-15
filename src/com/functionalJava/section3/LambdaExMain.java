ff2f068271d4927d4e0cbaa0775f6c3e8fbd6701
DongsooWilliamKim <kdstheace@naver.com>
2022-08-15 23:54:19 +0900

    imperative vs declarative programming

--- src/com/functionalJava/section3/LambdaExMain.java
+++ src/com/functionalJava/section3/LambdaExMain.java
@@ -1,27 +1,21 @@
 package com.functionalJava.section3;
 
+import java.util.stream.IntStream;
+
 public class LambdaExMain {
     public static void main(String ... args){
-        // 1. Using Outer Class
-        // MyRunnable runnable = new MyRunnable();
-        // Thread th1 = new Thread(runnable);
-        // th1.start();
+        //Imperative style
+        int sumOfEvens = 0;
+        for (int i = 0; i <= 100; i ++){
+            if(i % 2 ==0) sumOfEvens+=i;
+        }
+        System.out.println(sumOfEvens);
 
-        //2. Using Anonymous inner Class
-        Thread th2 = new Thread(new Runnable(){
-            @Override
-            public void run(){
-                System.out.println("Thread Executed");
-            }
-        });
-        th2.start();
-
-        //3. Lambda  (Runnable Interface only have a single abstract method)
-        //람다로 바꿀 수 있는 건 오직 추상 메소드가 하나만 있을 때 가능하다!!
-        Thread th3 = new Thread(() -> System.out.println("Thread Executed with Lambda"));
-        th3.start();
-
-        new Thread(() -> System.out.println("hello Thread")).start();
+        //Declarative or Functional Style
+        sumOfEvens = IntStream.rangeClosed(0, 100)
+            .filter(a -> a % 2 == 0)
+            .reduce((x,y)->x+y)
+            .getAsInt();
+        System.out.println(sumOfEvens);
     }
-
 }

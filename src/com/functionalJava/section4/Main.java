88ae1d292848e3796e68fb56d71ea4a3e417d380
DongsooWilliamKim <kdstheace@naver.com>
2022-08-16 00:04:05 +0900

    lambda practice
    lambda practice

---
+++ src/com/functionalJava/section4/Main.java
@@ -0,0 +1,9 @@
+package com.functionalJava.section4;
+
+public class Main {
+    public static void main(String ... args){
+        Name name = () -> System.out.println("sexysoo");
+        name.myName();
+    }
+
+}
---
+++ src/com/functionalJava/section4/Name.java
@@ -0,0 +1,6 @@
+package com.functionalJava.section4;
+@FunctionalInterface
+public interface Name {
+    void myName();
+
+}

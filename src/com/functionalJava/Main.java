5e80b3dd16ad22e35a25a5c0396e1f97a1bb9f21
DongsooWilliamKim <kdstheace@naver.com>
2022-08-22 00:08:57 +0900

    testPractice
    testPractice

--- src/com/functionalJava/Main.java
+++ src/com/functionalJava/Main.java
@@ -1,7 +1,20 @@
 package com.functionalJava;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-
+        Stream<Object> empty = Stream.empty();
+        System.out.println(empty.count());
     }
 }
+// ----1-----
+// hello
+// it's me
+// ----2-----

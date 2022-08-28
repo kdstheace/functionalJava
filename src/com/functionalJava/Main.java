ffcfdf9d3c9401402808d4e189b13d484705ce30
DongsooWilliamKim <kdstheace@naver.com>
2022-08-28 19:41:54 +0900

    OptionalReview
    OptionalReview

--- src/com/functionalJava/Main.java
+++ src/com/functionalJava/Main.java
@@ -1,19 +1,15 @@
 package com.functionalJava;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        Stream.of("bb", "aa", "bsd", "ccc", "ddd", "abc", "99", "Cbb", "Taa", "taa").sorted().forEach(a -> System.out.print(a + " "));
-        Stream<String> strStream = Stream.of("dd", "aaa", "CC", "cc", "b");
-        
-
+        Optional<String> helloOpt = Optional.<String>of("hello");
+        String hello = helloOpt.orElse("fire");
+        System.out.println(hello);
     }
 }
 // ----1-----
